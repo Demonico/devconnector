e760ba99fa3609d46056b87f414fea1f2c83da04
Jarrod <Demonico@users.noreply.github.com>
2018-04-21 19:43:21 -0600

    connected mongo and began creating api routes

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://jdvd:hem258@ds038379.mlab.com:38379/devconnect-jvd'
+}
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+// Import Routes
+const posts = require('./routes/api/posts')
+const profile = require('./routes/api/profile')
+const users = require('./routes/api/users')
+
+const app = express()
+
+// ================================================
+// DB Config
+const db = require('./config/keys').mongoURI
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err))
+
+// Define Port
+const port = process.env.PORT || 5000
+
+// ================================================
+// Begin Routes
+app.get('/', (req, res) => res.send('Hello'))
+
+// API Routes
+app.use('/api/posts', posts)
+app.use('/api/profile', profile)
+app.use('/api/users', users)
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
